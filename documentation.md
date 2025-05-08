9d263520bb939b2f2ddd3abe22fe66953968b347
jouleion <124351760+jouleion@users.noreply.github.com>
2025-05-08 12:14:11 +0200

    documentation and main.py

---
+++ documentation.md
@@ -0,0 +1,25 @@
+# Tasks
+In the github tasks
+- [ ] Read all local and foreign traffic on the network and distinguish them
+- [ ] Visualize the traffic as a pin thats on the map
+- [ ] Visualize the traffic in a cool lighting of map way
+- [ ] Host it on a website
+- [ ] Visualize all foreign traffic
+- [ ] Visualize our own foreign transmitter on the map
+
+
+# Moscow Analysis
+
+| Feature                                          | Moscow |
+|--------------------------------------------------|--------|
+| Read all local traffic on the network            | Must   |
+| Visualize all local the traffic                  | Must   |
+| Vizualize all locat traffic with light on map    | Should |
+| Host it on a website                             | Should |
+| Vizualize all foreign traffic                    | Could  |
+| Vizualize our own foreign transmitter on the map | Could  |
+
+
+
+
+
--- python/main.py
+++ python/main.py
@@ -2,6 +2,7 @@
 
 from websockets.sync.client import connect
 import json
+import pandas as pd
 
 def receive_data():
     max_msg = 10  # Feel free to change this or just let it loop forever. Maybe best to keep it low while testing...
@@ -32,6 +33,61 @@ def handle_message(msg):
         print("This is a known device!")
         print("Device name: ", msg['device_name'])
     print()
+
+    cross_reference_with_csv(msg)
+
+
+def cross_reference_with_csv(msg):
+    # Load data files
+    gate_way_locations = pd.read_csv('data/gateway_locations.csv')
+    sensor_locations = pd.read_csv('data/sensor_locations.csv')
+
+
+    # Get sensor data
+    sensor = sensor_locations[sensor_locations['Sensor_Eui'] == msg['device_eui']].iloc[0]
+    gateway_row = gate_way_locations[gate_way_locations['eui'] == msg['gateway'].replace(":", "")]
+    if not gateway_row.empty:
+        gateway = gateway_row.iloc[0]
+    else:
+        print("No matching gateway found!")
+        return
+
+    # Extract coordinates
+    sensor_lat, sensor_lon = sensor['St_X'], sensor['St_Y']
+    gateway_lat, gateway_lon = gateway['latitude'], gateway['longitude']
+
+    # Generate Leaflet map
+    html = f"""
+    <html>
+    <head>
+        <title>LoRa Device Map</title>
+        <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+        <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
+    </head>
+    <body>
+        <div id="map" style="height: 600px"></div>
+        <script>
+            var map = L.map('map').setView([{sensor_lat}, {sensor_lon}], 16);
+            L.tileLayer('https://{{s}}.tile.openstreetmap.org/{{z}}/{{x}}/{{y}}.png').addTo(map);
+
+            L.marker([{sensor_lat}, {sensor_lon}])
+                .bindPopup('Sensor: {msg['device_name']}')
+                .addTo(map);
+
+            L.marker([{gateway_lat}, {gateway_lon}])
+                .bindPopup('Gateway: {gateway['gateway_name']}')
+                .addTo(map);
+
+            L.polyline([[{sensor_lat}, {sensor_lon}], [{gateway_lat}, {gateway_lon}]], 
+                      {{color: 'red'}}).addTo(map);
+        </script>
+    </body>
+    </html>
+    """
+
+    with open('output/map.html', 'w') as f:
+        f.write(html)
+    print("Generated device map")
 
 
 if __name__ == "__main__":
