e56d4ab16546a1151a76237ada6c86cc5c871fe8
jouleion <124351760+jouleion@users.noreply.github.com>
2025-05-08 11:22:04 +0200

    Main.py

--- python/main.py
+++ python/main.py
@@ -1,3 +1,39 @@
-# hi
-# hi
-#hi
+# Fetch LoRa Signals
+
+from websockets.sync.client import connect
+import json
+
+def receive_data():
+    max_msg = 10  # Feel free to change this or just let it loop forever. Maybe best to keep it low while testing...
+    with connect("ws://192.87.172.71:1337") as websocket:
+        count = 0
+        while count < max_msg:
+            msg = websocket.recv()
+            try:
+                msg = json.loads(msg)
+                handle_message(msg)  # Call function to do the actual data handling
+            except json.JSONDecodeError:
+                print("Failed to decode json, assuming next packet will be ok...")
+                pass
+            except Exception as e:
+                # Assume something went wrong and stop receiving
+                print("Something went horribly wrong!")
+                print("Error:", e)
+                break
+
+            count += 1
+        print("Received %d messages!" % count)
+
+
+def handle_message(msg):
+    print("Received packet with rssi: %d." % msg['rssi'])
+    print(msg)
+    if 'device_eui' in msg:
+        print("This is a known device!")
+        print("Device name: ", msg['device_name'])
+    print()
+
+
+if __name__ == "__main__":
+    # Call the function to start receiving data
+    receive_data()
